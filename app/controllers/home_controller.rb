4ce841749512d5e605b7abeb9c80092bbde14cac
Wutt Yee Tun <wuttyeetun1721999@gmail.com>
2021-12-10 15:38:51 +0630

    initialize commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+class HomeController < ApplicationController
+  def index
+  end
+  def about
+    @about_me = "My Name is Crystal Braia..."#instance variable
+    @answer = 2 + 2 
+  end
+  def contact1
+    
+  end
+end
